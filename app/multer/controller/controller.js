401d63a8f43011d037f718a54f25c48fd763d33f
hardeepsingh34 <hardeepbhatti325.com>
2025-08-02 21:27:04 +0530

    2/8/2025 code

--- app/multer/controller/controller.js
+++ app/multer/controller/controller.js
@@ -1,6 +1,7 @@
 const multer = require('multer');
 const {v4: uuidv4} = require('uuid');
-// const path = require("path");
+const path = require("path");
+var userModel = require("../../user/models/model")
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
@@ -8,7 +9,7 @@ const storage = multer.diskStorage({
     },
     filename: function(req, file, cb){
         const uniquename = uuidv4();
-        cb( null, uniquename);
+        cb( null, uniquename+ path.extname(file.originalname));
     }
 });
 
@@ -19,5 +20,6 @@ exports.fileUpload = (req, res)=>{
         if(!req.file){
          return res.status(400).send('No files were uploded.');
         }
-        res.send('file send successfully!');
+        // jo file upload hui hai use save karo as a post and uska postid user ko do and post ko user ka id do 
+        
 }
--- app/multer/routes/routes.js
+++ app/multer/routes/routes.js
@@ -1,7 +1,8 @@
 const express = require('express')
 var router = express.Router();
 var upload = require('../controller/controller')
+const user = require("../../user/controller/controller")
 
-router.post('/upload',upload.upload.single("file"),upload.fileUpload );
+router.post('/upload',user.isLoggedIn ,upload.upload.single("file"),upload.fileUpload );
 
 module.exports = router;
